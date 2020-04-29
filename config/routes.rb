d737adc5c724b6677c23e4610ad28091d5efbf79
Hank Liu <hanke.liu@gmail.com>
2020-04-28 22:25:16 -0400

    create routes for registrations and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # root to: 'application#index'
+
   resources :movies
+
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
