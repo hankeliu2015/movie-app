cf29cd44519adcd6be94965c487ca86a4a26a75f
Hank Liu <hanke.liu@gmail.com>
2020-04-28 23:11:56 -0400

    create form_for for new user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # root to: 'application#index'
-
+  root 'movies#index'
+  
   resources :movies
-
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
