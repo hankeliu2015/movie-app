bdb4a65c9bde9dcbca0db98c06bf912e4ee64cf5
Hank Liu <hanke.liu@gmail.com>
2020-04-29 13:58:00 -0400

    add strong user params for retistration control create action. add user_id property to session after new user saved

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,6 +6,19 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
