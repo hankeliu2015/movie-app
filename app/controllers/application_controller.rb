e9f87a1f35b7c61f9d0f6d9204b6286662fe268f
Hank Liu <hanke.liu@gmail.com>
2020-04-29 15:52:22 -0400

    add helper method in applicaiton controller for the filters in movie controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  helper_methods :current_user, :is_logged_in, :authenticate_user!, :is_admin?
+
+  def index
+  end
+
+  private
+    def current_user
+      @current_user = User.find_by(id: session[:user_id])
+    end
+
+    def is_logged_in?
+      !!current_user
+    end
+
+    def authenticate_user!
+      redirect_to new_session_path if !is_logged_in?
+    end
+
+    def is_admin?
+      current_user.admin if current_user
+    end
+
+    
+
 end
