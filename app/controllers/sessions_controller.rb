056fd2b1d2a2c0cab01c0f50cf34757fbf562b49
Hank Liu <hanke.liu@gmail.com>
2020-04-29 22:00:24 -0400

    add new erb file for session. Session controller create action for use login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
-end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+end #end of class
