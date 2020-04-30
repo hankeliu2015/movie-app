5e036f44f4f408df52cbbf543f539354a8d0a85d
Hank Liu <hanke.liu@gmail.com>
2020-04-30 13:48:05 -0400

    commments clean up and format update in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,38 +2,9 @@ class MoviesController < ApplicationController
 
   before_action :authenticate_user!
   before_action :redirect_if_not_admin!, only: [:new, :create]
-  # non authenticated users should not be able to see any of these routes
-  # only authenticated users who are an admin can create , delete or edit a movie
-
-  # user model
-    # attributes
-    # username
-    # email
-    # user password
-    # admin: boolean dafault false <=regular user and admin
-
-  # registration and login system
-    # form to create an user account, with email, user and password
-      # user signup, user info update, user deletion
-
-    # session controller |  a form to login with email  and password
-      # user login and logout
-
-    # Userscontroller (optional)
-      # user show profile page
-      # employee page
-
-  # helper method
-    #is_logged_in?
-    #current_user
-    # authenticate_user! -> if a user is not logged , send them to a login screen
-
-  # sessions what are they?
 
   def index
     @movies = Movie.all
-    # binding.pry
-    # byebug
   end
 
   def new
